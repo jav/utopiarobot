1ebba915faf3c2de7dfb33b7e81dcb704896b3e9
Javier Ubillos <jav@spotify.com>
2012-06-17 09:55:20 +0200

    Added proper logging, splitted out the htmlparser.

---
+++ htmlparser.py
@@ -0,0 +1,58 @@
+import logging
+import sgmllib
+
+log = logging.getLogger(__name__)
+
+class Parser(sgmllib.SGMLParser):
+    "A simple parser class."
+
+    parserState = {}
+    parserState['form_depth'] = 0
+
+    login_info = {}
+
+    def __init__(self, verbose=0):
+        "Initialise an object, passing 'verbose' to the superclass."
+
+        sgmllib.SGMLParser.__init__(self, verbose)
+        self.hyperlinks = []
+
+    def parse(self, s):
+        "Parse the given string 's'."
+        self.feed(s)
+        self.close()
+
+    def start_form(self, attributes):
+        log.debug("start_form(%s)"%attributes)
+        self.parserState['form_depth'] += 1
+        for name, value in attributes:
+            if name == "action":
+                print "start_form(%s)"% attributes, value
+                self.parserState['login_form'] = self.parserState['form_depth']
+                log.debug(attributes)
+                self.login_info['form'] = dict(attributes)
+                log.debug( self.login_info['form'])
+
+    def end_form(self):
+        if 'login_form' in self.parserState.keys():
+            if self.parserState['login_form'] <= self.parserState['form_depth']:
+                self.parserState['login_form'] = -1
+
+        self.parserState['form_depth'] -= 1
+
+    def start_input(self, attributes):
+        if self.parserState['login_form'] > 0:
+            item = {}
+            for k, v in attributes:
+                item[k] = v
+            if 'name' in item.keys():
+                self.login_info[item['name']] = item
+            else:
+                if not 'other' in self.login_info or not isinstance( self.login_info['other'], list):
+                    self.login_info['other'] = []
+                self.login_info['other'].append(dict(item))
+
+
+
+    def get_login_fields(self):
+        return self.login_info
--- utopia.py
+++ utopia.py
@@ -1,86 +1,58 @@
 #!/usr/bin/env python
 
+import cookielib
 import inspect
 import logging
 from optparse import OptionParser
+import os
 import urllib
 import urllib2
 import re
-import sgmllib
 
+import htmlparser
 
 log = logging.getLogger(__name__)
 
-class MyParser(sgmllib.SGMLParser):
-    "A simple parser class."
+URL_BASE = "http://utopia-game.com"
+URL_LOGIN = "%s/shared" % URL_BASE
 
-    parserState = {}
-    parserState['form_depth'] = 0
-
-    login_info = {}
-
-    def __init__(self, verbose=0):
-        "Initialise an object, passing 'verbose' to the superclass."
-
-        sgmllib.SGMLParser.__init__(self, verbose)
-        self.hyperlinks = []
-
-    def parse(self, s):
-        "Parse the given string 's'."
-        self.feed(s)
-        self.close()
-
-    def start_form(self, attributes):
-        log.debug("start_form(%s)"%attributes)
-        self.parserState['form_depth'] += 1
-        for name, value in attributes:
-            if name == "action":
-                print "start_form(%s)"% attributes, value
-                self.parserState['login_form'] = self.parserState['form_depth']
-                log.debug(attributes)
-                self.login_info['form'] = dict(attributes)
-                log.debug( self.login_info['form'])
-
-    def end_form(self):
-        if 'login_form' in self.parserState.keys():
-            if self.parserState['login_form'] <= self.parserState['form_depth']:
-                self.parserState['login_form'] = -1
-
-        self.parserState['form_depth'] -= 1
-
-    def start_input(self, attributes):
-        log.debug("start_input()")
-        if self.parserState['login_form'] > 0:
-            item = {}
-            for k, v in attributes:
-                item[k] = v
-            if 'name' in item.keys():
-                self.login_info[item['name']] = item
-            else:
-                if not 'other' in self.login_info or not isinstance( self.login_info['other'], list):
-                    self.login_info['other'] = []
-                self.login_info['other'].append(dict(item))
-
-    def get_login_fields(self):
-        return self.login_info
+PAGE_LOGIN=1
+PAGE_CHOOSE_WORLD=2
+PAGE_CHOOSE_ACCOUNT=3
+PATE_THRONE=4
 
 class uplayer(object):
 
     password = ""
     username = ""
     user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.56 Safari/536.5"
+    cookie_file = "cookie_file.txt"
+    cj = False
 
     def __init__(self):
-        pass
+        self.cj = cookielib.LWPCookieJar()
+        if os.path.isfile(self.cookie_file):
+            self.cj.load(self.cookie_file)
+
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
+        urllib2.install_opener(opener)
 
     def do_login(self):
+        log.debug("do_login")
         headers = { 'User-Agent' : self.user_agent }
         values= {}
         data = urllib.urlencode(values)
-        req = urllib2.Request("http://utopia-game.com/shared", data, headers)
+        log.debug("Loading page: %s" % URL_LOGIN)
+        req = urllib2.Request(URL_LOGIN, data, headers)
         res = urllib2.urlopen(req)
 
-        myparser = MyParser()
+        if log.level <= 10 and self.cj:
+            log.debug("Cookies")
+            for index, cookie in enumerate(self.cj):
+                log.debug("%d: %s" % (index, cookie))
+        self.cj.save(self.cookie_file)
+
+        myparser = htmlparser.Parser()
         myparser.parse(res.read())
 
         login_fields = myparser.get_login_fields()
@@ -88,30 +60,32 @@ class uplayer(object):
 
         post = {}
         for k,v in login_fields.items():
-            print "login_fields - k:%s, v:%s" % (k,v)
             if 'other' == k:
                 pass
             elif 'name' in v.keys():
                 print "TYPE", v['type']
-                if 'password' == v['type']:
-                    v['value'] = '123'
+                if 'password' == v['name']:
+                    v['value'] = self.password
                     login_fields[k]['value'] = v['value']
-                if 'username' == v['type']:
-                    v['value'] = 'jav'
+                if 'username' == v['name']:
+                    v['value'] = self.username
                     login_fields[k]['value'] = v['value']
             post[k] = v
 
-        log.debug( "login_fields", login_fields )
+        log.debug( "login_fields %s" % login_fields )
 
-        value = {}
+        values = {}
         for k,v in login_fields.items():
-            if 'name' == k:
-                value[k] = v
+            log.debug("k: %s, v: %s" %( k,v))
+            if isinstance(v, dict) and 'name' in v.keys():
+                values[v['name']] = v['value']
 
-        log.debug( "value", value )
+        log.debug( "values %s", values )
 
         data = urllib.urlencode(values)
-        req = urllib2.Request(login_fields['form']['action'], data, headers)
+        URL_TAKE_LOGIN = "%s%s" % (URL_BASE, login_fields['form']['action'])
+        log.debug("accessing %s POST:%s" % (URL_TAKE_LOGIN, data) )
+        req = urllib2.Request(URL_TAKE_LOGIN, data, headers)
         res = urllib2.urlopen(req)
         log.debug(res.read())
 
@@ -121,13 +95,12 @@ if __name__ == "__main__":
         parser = OptionParser()
         parser.add_option("-f", "--file", dest="filename",
                           help="Insturctions file (not yet implemented)")
-        parser.add_option("-d", "--debug",
-                          action="store_true", dest="debug", default=False,
-                          help="Print lots of debug logging")
+        parser.add_option("-d", "--debug", action="store_true", dest="debug",
+                          default=False, help="Print lots of debug logging")
         parser.add_option("-l", "--log-level", dest="log_level", default=logging.WARN)
-        parser.add_option("-c", "--config", help="Config file to read from.")
-        parser.add-option("-u", "--username", help="Username for player.")
-        parser.add-option("-p", "--password", help="Password for player. (can be defined in conf too)")
+        parser.add_option("-c", "--config", help="Config file to read from (not implemented).")
+        parser.add_option("-u", "--username", help="Username for player.")
+        parser.add_option("-p", "--password", help="Password for player. (can be defined in conf too)")
         (options, args) = parser.parse_args()
 
         if(options.debug):
@@ -139,6 +112,13 @@ if __name__ == "__main__":
         #Actual logic
         log.debug("Instanciating player")
         player = uplayer()
-        log.info("Log in player")
+
+        player.username = options.username
+        log.debug("set username = %s" % player.username)
+
+        player.password = options.password
+        log.debug("set password = %s (masked)" % "".join(["*" for c in player.password]))
+
+        log.info("Log in player (%s)...", player.username)
         player.do_login()
 
