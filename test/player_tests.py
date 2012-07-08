8ec362676a74cef0640d630c3d0a277b66ccf6d4
Javier Ubillos <jav@spotify.com>
2012-07-08 10:29:01 +0200

    Revert "Removed redundand paching of 'nav' in tests."
    This reverts commit 1ae56ce7c1aea0d0a6d11849c19b8097224ddccf.

--- test/player_tests.py
+++ test/player_tests.py
@@ -258,19 +258,21 @@ class player_tests(object):
         assert(  0 == buildings['Dungeons']['incoming'])
 
 
-    @mock.patch('urllib2.urlopen')
-    @mock.patch('urllib2.Request')
-    @mock.patch.object(UtopiaRobot,'cache_page')
-    @mock.patch.object(UtopiaRobot,'_simulate_wait')
-    def test_train_troops(self, mock_simulate_wait, mock_cache_page, mock_request, mock_urlopen):
-        mock_urlopen.return_value = mock_request
-        mock_request.read.return_value = open('test/growth_built.html').read()
-        mock_cache_page.return_value = True
-        mock_simulate_wait.return_value = True
+    # @mock.patch('urllib2.urlopen')
+    # @mock.patch('urllib2.Request')
+    # @mock.patch.object(htmlparser.GrowthParser, 'get_nav_links')
+    # @mock.patch.object(UtopiaRobot,'cache_page')
+    # @mock.patch.object(UtopiaRobot,'_simulate_wait')
+    # def test_train_troops(self, mock_simulate_wait, mock_cache_page, mock_militaryparser_nav, mock_request, mock_urlopen):
+    #     mock_urlopen.return_value = mock_request
+    #     mock_request.read.return_value = open('test/growth_built.html').read()
+    #     mock_militaryparser_nav.return_value = {'Military': '/wol/game/build'}
+    #     mock_cache_page.return_value = True
+    #     mock_simulate_wait.return_value = True
 
-        buildings={}
+    #     buildings={}
 
-        print "Want to train: %s" % military
-        military_result = self.player.train_military(military)
-        print "Militarty result: %s" % military_result
-        assert(44 == military_result['o-spec'])
+    #     print "Want to train: %s" % military
+    #     military_result = self.player.train_military(military)
+    #     print "Militarty result: %s" % military_result
+    #     assert(44 == military_result['o-spec'])
